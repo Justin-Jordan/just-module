3dd062b4be9cdee1dea6e40c6b318ac90786bdd8
Justin Onomo <justinonomo@gmail.com>
2022-05-04 12:07:24 +0200

    make dynamic plumbing bg section

--- src/Plugin/Layout/Sections/PlumbingBgFixed.php
+++ src/Plugin/Layout/Sections/PlumbingBgFixed.php
@@ -3,6 +3,7 @@
 namespace Drupal\just_mod\Plugin\Layout\Sections;
 
 use Drupal\Core\Layout\LayoutDefault;
+use Drupal\formatage_models\FormatageModelsThemes;
 use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
 
 /**
@@ -15,16 +16,63 @@ use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
  *   path = "layouts/sections",
  *   template = "plumbing_bg_fixed",
  *   library = "just_mod/plumbing_bg_fixed",
+ * regions = {
+ *     "title" = {
+ *       "label" = @Translation("Main title"),
+ *     },
+ *     "subTitle" = {
+ *       "label" = @Translation("sub title")
+ *     },
+ *     "callToAction" = {
+ *       "label" = @Translation("Call to action")
+ *     }
+ *   }
  * )
  */
 class PlumbingBgFixed extends FormatageModelsSection
 {
-    /**
-     *
-     * {@inheritdoc}
-     */
+    public function build(array $regions)
+  {
+    $build = parent::build($regions);
+    FormatageModelsThemes::formatSettingValues($build);
+    return $build;
+  }
+
     public function defaultConfiguration()
-    {
-        return parent::defaultConfiguration();
-    }
+  {
+    return parent::defaultConfiguration() + [
+      'sf' => [
+        'builder-form' => true,
+        'info' => [
+          'title' => 'Contenu',
+          'loader' => 'static'
+        ],
+        'fields' => [
+          'title' => [
+            'text' => [
+              'label' => 'Title',
+              'value' => "Free estimates are provided for work that would be
+              more involved than a service call."
+            ]
+          ],
+          'subTitle' => [
+            'text' => [
+              'label' => 'Sub title',
+              'value' => "Remodeling, Replacing water
+              or sewer lines, Replacing Fixtures, etc."
+            ]
+          ],
+          'callToAction' => [
+            'url' => [
+              'label' => 'Call action',
+              'value' => [
+                'link' => '#',
+                'text' => 'request an estimate',
+              ]
+            ]
+          ]
+        ]
+      ]
+    ];
+  }
 }
--- src/Plugin/Layout/Sections/PlumbingHelpSection.php
+++ src/Plugin/Layout/Sections/PlumbingHelpSection.php
@@ -38,61 +38,63 @@ use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
 class PlumbingHelpSection extends FormatageModelsSection
 {
 
-    public function build(array $regions) {
-        $build = parent::build($regions);
-        FormatageModelsThemes::formatSettingValues($build);
-        return $build;
-      }
+  public function build(array $regions)
+  {
+    $build = parent::build($regions);
+    FormatageModelsThemes::formatSettingValues($build);
+    return $build;
+  }
 
-    public function defaultConfiguration() {
-        return parent::defaultConfiguration() + [
-          'sf' => [
-            'builder-form' => true,
-            'info' => [
-              'title' => 'Contenu',
-              'loader' => 'static'
-            ],
-            'fields' => [
-              'title' => [
-                'text' => [
-                  'label' => 'Title',
-                  'value' => "Plumbing Problem?"
-                ]
-              ],
-              'subTitle' => [
-                'text' => [
-                  'label' => 'Sub title',
-                  'value' => "We can help you"
-                ]
-              ],
-              'descriptionTitle' => [
-                'text' => [
-                  'label' => 'Description Title',
-                  'value' => "Professional Plumbing, Leak and Heating Services"
-                ]
-              ],
-              'descriptionTitle' => [
-                'text_html' => [
-                  'label' => 'Description',
-                  'value' => '
+  public function defaultConfiguration()
+  {
+    return parent::defaultConfiguration() + [
+      'sf' => [
+        'builder-form' => true,
+        'info' => [
+          'title' => 'Contenu',
+          'loader' => 'static'
+        ],
+        'fields' => [
+          'title' => [
+            'text' => [
+              'label' => 'Title',
+              'value' => "Plumbing Problem?"
+            ]
+          ],
+          'subTitle' => [
+            'text' => [
+              'label' => 'Sub title',
+              'value' => "We can help you"
+            ]
+          ],
+          'descriptionTitle' => [
+            'text' => [
+              'label' => 'Description Title',
+              'value' => "Professional Plumbing, Leak and Heating Services"
+            ]
+          ],
+          'descriptionTitle' => [
+            'text_html' => [
+              'label' => 'Description',
+              'value' => '
                   <p>Whether you’re planning a renovation or simply need routine maintenance
                               on your home or
                               office plumbing systems, our
                               professional technicians and mechanics can assist you effectively in the right way.</p>
                   '
-                ]
-              ],
-              'callToAction' => [
-                'url' => [
-                  'label' => 'Call action',
-                  'value' => [
-                    'link' => '#',
-                    'text' => 'request a plomber',
-                  ]
-                ]
+            ]
+          ],
+          'callToAction' => [
+            'url' => [
+              'label' => 'Call action',
+              'value' => [
+                'link' => '#',
+                'text' => 'request a plomber',
               ]
             ]
           ]
-        ];
-      }
+        ]
+      ]
+    ];
+  }
 }
